bc5ff580276539ce9263f1d9a14ebf2459f5561f
Brian Vu <emailbrian.vu@gmail.com>
2022-11-10 14:05:29 -0800

    refactor: imported install.sh

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
 
+: "${OSD_FAMILY:="linux"}"
+: "${HOSTTYPE:="amd64"}"
+if [ "${HOSTTYPE}" = "x86_64" ]; then HOSTTYPE="amd64"; fi
+case "${HOSTTYPE}" in *86 ) HOSTTYPE=i386 ;; esac
+
+if command -v go >/dev/null; then
+    if go version | grep -q -F "go<< parameters.version >> "; then
+    echo "Binary already exists, skipping download."
+    exit 0
+    fi
+
+    echo "Found a different version of Go."
+    OSD_FAMILY="$(go env GOHOSTOS)"
+    HOSTTYPE="$(go env GOHOSTARCH)"
+
+    $SUDO rm -rf /usr/local/go
+    $SUDO install --owner="${USER}" -d /usr/local/go
+fi
+
+echo "Installing the requested version of Go."
+
+curl --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" \
+| sudo tar --no-same-owner --strip-components=1 --gunzip -x -C /usr/local/go/
+
+echo "export PATH=$PATH:/usr/local/go/bin" >> "$BASH_ENV"
+$SUDO chown -R "$(whoami)": /usr/local/go
 
