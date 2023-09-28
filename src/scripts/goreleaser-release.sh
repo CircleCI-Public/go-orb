2beac78b6eb4b52b53b9251258e415032183215b
Brian Vu <emailbrian.vu@gmail.com>
2023-09-28 00:06:58 -0700

    ci: add app for testing

--- src/scripts/goreleaser-release.sh
+++ src/scripts/goreleaser-release.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-GITHUB_TOKEN="$(echo "\$$GITHUB_TOKEN" | circleci env subst)"
 
-if [ -n "${GITHUB_TOKEN}" ]; then
+GITHUB_TOKEN="$(echo "\$$GITHUB_TOKEN" | circleci env subst)"
+GO_EVAL_PROJECT_PATH="$(eval echo "${GO_EVAL_PROJECT_PATH}")"
+
+# Change to directory containing project files
+cd "${GO_EVAL_PROJECT_PATH}" || return
+
+if [ -z "${GITHUB_TOKEN}" ]; then
     echo "No GitHub Token provided. Please add token as environment variable in CircleCI."
+    exit 1
 fi
 
 if [ "$GO_BOOL_VALIDATE_YAML" -eq 1 ]; then 
@@ -17,4 +23,5 @@ else
     # Build binaries and test release locally
     goreleaser release --snapshot --clean
 fi
+
 
