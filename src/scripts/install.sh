ec8ae4b7fd1e0b23e092fe939c033a76f07711f7
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-01-09 11:04:27 -0500

    fix: using the correct TAR options for Alpine Linux distro

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -4,28 +4,32 @@
 : "${HOSTTYPE:="amd64"}"
 if [ "${HOSTTYPE}" = "x86_64" ]; then HOSTTYPE="amd64"; fi
 if [ "${HOSTTYPE}" = "aarch64" ]; then HOSTTYPE="arm64"; fi
-case "${HOSTTYPE}" in *86 ) HOSTTYPE=i386 ;; esac
+case "${HOSTTYPE}" in *86) HOSTTYPE=i386 ;; esac
 
 if command -v go >/dev/null; then
-    if go version | grep -q -F "go<< parameters.version >> "; then
+  if go version | grep -q -F "go<< parameters.version >> "; then
     echo "Binary already exists, skipping download."
     exit 0
-    fi
+  fi
 
-    echo "Found a different version of Go."
-    OSD_FAMILY="$(go env GOHOSTOS)"
-    HOSTTYPE="$(go env GOHOSTARCH)"
+  echo "Found a different version of Go."
+  OSD_FAMILY="$(go env GOHOSTOS)"
+  HOSTTYPE="$(go env GOHOSTARCH)"
 
-    $SUDO rm -rf /usr/local/go
-    $SUDO install --owner="${USER}" -d /usr/local/go
+  $SUDO rm -rf /usr/local/go
+  $SUDO install --owner="${USER}" -d /usr/local/go
+fi
+
+# Checking if Alpine is being used to update the tar options
+TAR_OPTIONS="--no-same-owner --strip-components=1 --gunzip -x -C"
+if [[ $(cat /etc/os-release | grep '^NAME=' | cut -d'=' -f2) = "Alpine Linux" ]]; then
+  TAR_OPTIONS="-o --strip-components=1 -z -x -C"
 fi
 
 echo "Installing the requested version of Go."
-
-curl --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" \
-| $SUDO tar --no-same-owner --strip-components=1 --gunzip -x -C /usr/local/go/
+curl --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" |
+  $SUDO tar "${TAR_OPTIONS}" /usr/local/go/
 
 #shellcheck disable=SC2016
-echo 'export PATH=$PATH:/usr/local/go/bin' >> "$BASH_ENV"
+echo 'export PATH=$PATH:/usr/local/go/bin' >>"$BASH_ENV"
 $SUDO chown -R "$(whoami)": /usr/local/go
-
