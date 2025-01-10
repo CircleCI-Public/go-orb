dcc4308879f50dd601d6576c88f1bbd07c839e51
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-01-09 21:45:31 -0500

    fix: correctly installing go and gofmt to /usr/local/bin

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -22,14 +22,11 @@ fi
 
 # Checking if Alpine is being used to update the tar options
 echo "Installing the requested version of Go."
-if grep -q 'Alpine Linux' /etc/os-release; then
-  curl --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" |
-    $SUDO tar -o --strip-components=1 -z -x -C /usr/local/go/
-else
-  curl --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" |
-    $SUDO tar --no-same-owner --strip-components=1 --gunzip -x -C /usr/local/go/
-fi
+curl -O --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
+$SUDO tar xzf go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz -C /opt/go
+rm go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz
+ln -s /opt/go/bin/go /usr/local/bin/go
+ln -s /opt/go/bin/gofmt /usr/local/bin/gofmt
 
 #shellcheck disable=SC2016
-echo 'export PATH=$PATH:/usr/local/go/bin' >>"$BASH_ENV"
-$SUDO chown -R "$(whoami)": /usr/local/go
+$SUDO chown -R "$(whoami)": /usr/local/bin/go /usr/local/bin/gofmt
