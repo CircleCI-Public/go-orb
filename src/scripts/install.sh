2bbe7d3ca53aa49566e8177c65795de9d883d42e
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-01-17 11:13:55 -0500

    fix: adding exit condition if cd commands fail

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 function alpine_install {
-  cd /opt
+  cd /opt || exit 200
   apk add bash gcc musl-dev go
   wget "https://go.dev/dl/go${ORB_VAL_VERSION}.src.tar.gz"
   tar xzf "go${ORB_VAL_VERSION}.src.tar.gz"
   mv go "go${ORB_VAL_VERSION}"
-  cd "go${ORB_VAL_VERSION}/src"
+  cd "go${ORB_VAL_VERSION}/src" || exit 201
   ./make.bash
   apk del go
   ln -s "/opt/go${ORB_VAL_VERSION}/bin/go" /usr/local/bin/go
