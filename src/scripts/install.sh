ddeb64957941eab4f6db2f6cb6581c6a75e4c2a1
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-01-17 11:10:12 -0500

    feat: install go specific version in Alpine by building the binary

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,32 +1,53 @@
 #!/usr/bin/env bash
 
+function alpine_install {
+  cd /opt
+  apk add bash gcc musl-dev go
+  wget https://go.dev/dl/go${ORB_VAL_VERSION}.src.tar.gz
+  tar xzf go${ORB_VAL_VERSION}.src.tar.gz
+  mv go go${ORB_VAL_VERSION}
+  cd go${ORB_VAL_VERSION}/src
+  ./make.bash
+  apk del go
+  ln -s /opt/go${ORB_VAL_VERSION}/bin/go /usr/local/bin/go
+  ln -s /opt/go${ORB_VAL_VERSION}/bin/gofmt /usr/local/bin/gofmt
+}
+
+function standard_install {
+  if command -v go >/dev/null; then
+    if go version | grep -q -F "go<< parameters.version >> "; then
+      echo "Binary already exists, skipping download."
+      exit 0
+    fi
+
+    echo "Found a different version of Go."
+    OSD_FAMILY="$(go env GOHOSTOS)"
+    HOSTTYPE="$(go env GOHOSTARCH)"
+
+    $SUDO rm -rf /usr/local/go
+    $SUDO install --owner="${USER}" -d /usr/local/go
+  fi
+
+  echo "Installing the requested version of Go."
+  curl -O --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
+  $SUDO tar xzf "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" -C /opt
+  rm "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
+  ln -s /opt/go/bin/go /usr/local/bin/go
+  ln -s /opt/go/bin/gofmt /usr/local/bin/gofmt
+
+  #shellcheck disable=SC2016
+  $SUDO chown -R "$(whoami)": /usr/local/bin/go /usr/local/bin/gofmt
+}
+
 : "${OSD_FAMILY:="linux"}"
 : "${HOSTTYPE:="amd64"}"
 if [ "${HOSTTYPE}" = "x86_64" ]; then HOSTTYPE="amd64"; fi
 if [ "${HOSTTYPE}" = "aarch64" ]; then HOSTTYPE="arm64"; fi
 case "${HOSTTYPE}" in *86) HOSTTYPE=i386 ;; esac
 
-if command -v go >/dev/null; then
-  if go version | grep -q -F "go<< parameters.version >> "; then
-    echo "Binary already exists, skipping download."
-    exit 0
-  fi
-
-  echo "Found a different version of Go."
-  OSD_FAMILY="$(go env GOHOSTOS)"
-  HOSTTYPE="$(go env GOHOSTARCH)"
-
-  $SUDO rm -rf /usr/local/go
-  $SUDO install --owner="${USER}" -d /usr/local/go
+grep alpinelinux /etc/os-release
+if [ $? -eq 0 ]; then
+  alpine_install
+else
+  standard_install
 fi
-
-# Checking if Alpine is being used to update the tar options
-echo "Installing the requested version of Go."
-curl -O --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
-$SUDO tar xzf "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" -C /opt
-rm "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
-ln -s /opt/go/bin/go /usr/local/bin/go
-ln -s /opt/go/bin/gofmt /usr/local/bin/gofmt
-
-#shellcheck disable=SC2016
-$SUDO chown -R "$(whoami)": /usr/local/bin/go /usr/local/bin/gofmt
