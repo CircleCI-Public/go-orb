6f2f5a56fc1056062de89c25337eafb312a8da8b
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-01-17 11:28:35 -0500

    fix: adding $SUDO in ln commands

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -31,9 +31,9 @@ function standard_install {
   echo "Installing the requested version of Go."
   curl -O --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
   $SUDO tar xzf "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" -C /opt
-  rm "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
-  ln -s /opt/go/bin/go /usr/local/bin/go
-  ln -s /opt/go/bin/gofmt /usr/local/bin/gofmt
+  $SUDO rm "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
+  $SUDO ln -s /opt/go/bin/go /usr/local/bin/go
+  $SUDO ln -s /opt/go/bin/gofmt /usr/local/bin/gofmt
 
   #shellcheck disable=SC2016
   $SUDO chown -R "$(whoami)": /usr/local/bin/go /usr/local/bin/gofmt
