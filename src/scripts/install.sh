7dee23cb11aeaa95c451eb2f44038bee60ae3ef5
David Alberto Montaño Fetecua <david.montano@circleci.com>
2025-01-21 09:46:53 -0500

    Adding support to install different go versions in Alpine (#100)
    * test: checking if istall fails in alpine
    
    * fix: using the correct TAR options for Alpine Linux distro
    
    * fix: removing useless cat
    
    * fix: removing doulbe quotes from tar options
    
    * fix: using bash arrays instead of a string for TAR options
    
    * fix: fixing typo
    
    * fix: adding curl dependency
    
    * fix: given that sh is used in Alpine, we need to remove usage of arrays
    
    * fix: updating the condition to prevent issues with sh
    
    * fix: adding double quotes
    
    * fix: adding -q to grep to silent output
    
    * fix: checking result of previous command to test if running in Alpine
    
    * fix: fixing check in Alpine using sh
    
    * fix: correctly installing go and gofmt to /usr/local/bin
    
    * fix: adding double quotes
    
    * fix: removing go from /opt/go given that the directory does not exist but /opt does
    
    * feat: install go specific version in Alpine by building the binary
    
    * fix: adding double quotes to env vars
    
    * fix: adding exit condition if cd commands fail
    
    * fix: adding command into the if check
    
    * fix: remove brackets from test
    
    * fix: adding $SUDO in ln commands
    
    * fix: adding f to ln to make sure the command does not fail
    
    * fix: fix test which was checking for python now it checks for the correct expected version of go
    
    * fix: fixing test by removing 1.16 given that this version does not have memstat.gc_sys defined
    
    * fix: trying to fix a regression tesdt with caches which is failling now
    
    * fix: trying with same version from previous cache
    
    * fix: trying to go back to the previous state and behavior of the file
    
    * fix: removing previously installed version of go

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
-: "${OSD_FAMILY:="linux"}"
-: "${HOSTTYPE:="amd64"}"
-if [ "${HOSTTYPE}" = "x86_64" ]; then HOSTTYPE="amd64"; fi
-if [ "${HOSTTYPE}" = "aarch64" ]; then HOSTTYPE="arm64"; fi
-case "${HOSTTYPE}" in *86 ) HOSTTYPE=i386 ;; esac
+function alpine_install {
+  cd /opt || exit 200
+  apk add bash gcc musl-dev go
+  wget "https://go.dev/dl/go${ORB_VAL_VERSION}.src.tar.gz"
+  tar xzf "go${ORB_VAL_VERSION}.src.tar.gz"
+  mv go "go${ORB_VAL_VERSION}"
+  cd "go${ORB_VAL_VERSION}/src" || exit 201
+  ./make.bash
+  apk del go
+  ln -sf "/opt/go${ORB_VAL_VERSION}/bin/go" /usr/local/bin/go
+  ln -sf "/opt/go${ORB_VAL_VERSION}/bin/gofmt" /usr/local/bin/gofmt
+}
 
-if command -v go >/dev/null; then
+function standard_install {
+  if command -v go >/dev/null; then
     if go version | grep -q -F "go<< parameters.version >> "; then
-    echo "Binary already exists, skipping download."
-    exit 0
+      echo "Binary already exists, skipping download."
+      exit 0
     fi
 
     echo "Found a different version of Go."
@@ -18,14 +26,28 @@ if command -v go >/dev/null; then
 
     $SUDO rm -rf /usr/local/go
     $SUDO install --owner="${USER}" -d /usr/local/go
+    $SUDO rm -rf /opt/go
+  fi
+
+  echo "Installing the requested version of Go."
+  curl -O --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
+  $SUDO tar xzf "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" -C /opt
+  $SUDO rm "go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz"
+  $SUDO ln -sf /opt/go/bin/go /usr/local/bin/go
+  $SUDO ln -sf /opt/go/bin/gofmt /usr/local/bin/gofmt
+
+  #shellcheck disable=SC2016
+  $SUDO chown -R "$(whoami)": /usr/local/bin/go /usr/local/bin/gofmt
+}
+
+: "${OSD_FAMILY:="linux"}"
+: "${HOSTTYPE:="amd64"}"
+if [ "${HOSTTYPE}" = "x86_64" ]; then HOSTTYPE="amd64"; fi
+if [ "${HOSTTYPE}" = "aarch64" ]; then HOSTTYPE="arm64"; fi
+case "${HOSTTYPE}" in *86) HOSTTYPE=i386 ;; esac
+
+if grep alpinelinux /etc/os-release; then
+  alpine_install
+else
+  standard_install
 fi
-
-echo "Installing the requested version of Go."
-
-curl --fail --location -sS "https://dl.google.com/go/go${ORB_VAL_VERSION}.${OSD_FAMILY}-${HOSTTYPE}.tar.gz" \
-| $SUDO tar --no-same-owner --strip-components=1 --gunzip -x -C /usr/local/go/
-
-#shellcheck disable=SC2016
-echo 'export PATH=$PATH:/usr/local/go/bin' >> "$BASH_ENV"
-$SUDO chown -R "$(whoami)": /usr/local/go
-
