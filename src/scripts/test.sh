f525d3b7553ab86d92f2d5cc0064511351a2f77f
Johan Sjöro <johan.sjoro@timeedit.com>
2025-03-05 15:49:50 +0100

    Properly check boolean flags in test command (#101)
    * fix: Properly check boolean flags in test command
    
    Previous code only checked if the env variable was set.
    As it is always set, the race, short and verbose options where always on.
    
    * fix: Use numeric comparison instead of string

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -20,20 +20,20 @@ fi
 
 COVER_PROFILE=$(eval echo "$ORB_EVAL_COVER_PROFILE")
 
-if [ -n "$ORB_VAL_RACE" ]; then
+if [ "$ORB_VAL_RACE" -eq 1 ]; then
   set -- "$@" -race
   ORB_VAL_COVER_MODE=atomic
 fi
 
-if [ "$ORB_VAL_FAILFAST" != "false" ]; then
+if [ "$ORB_VAL_FAILFAST" -eq 1 ]; then
   set -- "$@" -failfast
 fi
 
-if [ -n "$ORB_VAL_SHORT" ]; then
+if [ "$ORB_VAL_SHORT" -eq 1 ]; then
   set -- "$@" -short
 fi
 
-if [ -n "$ORB_VAL_VERBOSE" ]; then
+if [ "$ORB_VAL_VERBOSE" -eq 1 ]; then
   set -- "$@" -v
 fi
 
