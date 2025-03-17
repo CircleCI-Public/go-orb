d907a3f9c7b8599d30c255a4456d9de15215957f
Patrick Guinard <patrick@public.com>
2025-03-17 07:11:02 -0600

    chore: BREAKING CHANGE install goreleaser v2

--- src/scripts/install-goreleaser.sh
+++ src/scripts/install-goreleaser.sh
@@ -2,5 +2,5 @@
 
 GO_STR_VERSION="$(echo "${GO_STR_VERSION}"| circleci env subst)"
 
-go install github.com/goreleaser/goreleaser@"${GO_STR_VERSION}"
+go install github.com/goreleaser/goreleaser/v2@"${GO_STR_VERSION}"
 goreleaser --version
